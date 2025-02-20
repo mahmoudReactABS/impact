3cc1d67e690e9c3c691de03d7389924bff4df775
mahmoudReactABS <mmotaz.absai@yahoo.com>
2025-02-20 16:23:25 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const flowbite = require("flowbite-react/tailwind");
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    './public/index.html',
+    flowbite.content(),
+  ],
+  
+  plugins: [flowbite.plugin(),],
+};
